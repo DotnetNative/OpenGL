e65b88f0be5b1db747e359e573e18217f628d14e
Yotic <yoticcitoy@gmail.com>
2023-03-12 23:05:33 +0300

    Changed modifiers of Interop class

--- OpenGL/Interop.cs
+++ OpenGL/Interop.cs
@@ -6,11 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace OpenGL;
-public class Interop
+internal class Interop
 {
     [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
+    internal static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
 
     [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    internal static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 }
